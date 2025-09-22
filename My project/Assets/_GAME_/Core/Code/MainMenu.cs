c3303ae2044892eabbac53f9264ec99f99ca9b4f
shiara <shiara.arauzo@gmail.com>
2025-09-22 01:40:30 -0500

    player movement + play button

--- My project/Assets/_GAME_/Core/Code/MainMenu.cs
+++ My project/Assets/_GAME_/Core/Code/MainMenu.cs
@@ -4,10 +4,13 @@ using UnityEngine.SceneManagement;
 public class MainMenu: MonoBehaviour
 {
     public void Play()
-    {}
+    {
+         SceneManager.LoadScene("PrincipalMap");
+    }
 
     public void Quit()
     {
+        Debug.Log("Quit Game"); 
         Application.Quit();
     }
 
