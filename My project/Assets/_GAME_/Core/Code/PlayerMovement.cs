310d4df7d81024f92c9181303c72fde0e73bda11
shiara <shiara.arauzo@gmail.com>
2025-10-04 04:24:43 -0500

    add 4x player animations

--- My project/Assets/_GAME_/Core/Code/PlayerMovement.cs
+++ My project/Assets/_GAME_/Core/Code/PlayerMovement.cs
@@ -6,21 +6,37 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-    private Rigidbody2D rb; 
+    private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); 
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+
+
     }
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
     }
+
 }
