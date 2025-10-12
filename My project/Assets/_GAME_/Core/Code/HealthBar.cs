3d0bf33f0103d68ac644516de4bac51ba5d0a724
shiara <shiara.arauzo@gmail.com>
2025-10-11 21:54:55 -0500

    add health bar numbers

--- My project/Assets/_GAME_/Core/Code/HealthBar.cs
+++ My project/Assets/_GAME_/Core/Code/HealthBar.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    public void SetMaxHealth(int health)
     {
+        slider.maxValue = health;
+        slider.value = health;
+       fill.color= gradient.Evaluate(1f);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetHealth(int health)
     {
-        
-    }
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+     }
+
 }
